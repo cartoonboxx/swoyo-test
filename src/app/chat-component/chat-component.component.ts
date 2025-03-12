1b15424952a424bfe0847e9b3f7da18694fb1de1
cartoonboxx <shulenin_2005@mail.ru>
2025-03-12 14:48:03 +0300

    1.1

--- src/app/chat-component/chat-component.component.ts
+++ src/app/chat-component/chat-component.component.ts
@@ -20,22 +20,31 @@ export class ChatComponentComponent {
   public message: string = '';
   public messages: { username: string; message: string, date: Date }[] = this._localStorage.has('messages') ? JSON.parse(this._localStorage.get('messages')) : [];
 
-  public currentDate: Date = new Date();
+  // public isYour = JSON.parse(this._localStorage.get('userData')).username 'your'
+  public isYour: string = '';
 
   constructor() {
     console.log(this.messages)
   }
 
-  public alertMessage(event: any): void {
-    event.preventDefault();
+  public sendAndSaveMessage(event: Event | KeyboardEvent): void {
+    // event [KeyboardEvent | Submit]
+    event.preventDefault(); // Останавливаем стандартное поведение формы
 
-    this.currentDate = new Date();
+    if (event instanceof KeyboardEvent && event.key !== "Enter") {
+      return; // Если это не Enter, выходим
+    }
+
+    if (!this.message.trim()) {
+      return; // Если сообщение пустое — не отправляем
+    }
+
     const userData = JSON.parse(this._localStorage.get('userData'))
     console.log(userData);
     this.messages.push({
       username: userData.username,
-      message: this.message,
-      date: this.currentDate
+      message: this.message.trim(),
+      date: new Date()
     })
 
     this.message = '';
